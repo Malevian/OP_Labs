b7267c3bb8fd73ee999a17abdde0904157b62056
Чінь Андрій <achyn2004@gmail.com>
2021-09-27 15:08:13 +0300

    Add project files.

---
+++ Lab1/Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	float a, S, V;
+	cin >> a;
+	if (a > 0) {
+
+		S = 4 * a * a;
+		cout << "\nSurface area of cube:" << S << endl;
+		V = a * a * a;
+		cout << "\nVolume of cube :" << V << endl;
+	}
+	else if (a <= 0)
+	{
+		cout << "\n Action unavailable!" << endl;
+	}
+
+	return 0;
+}
